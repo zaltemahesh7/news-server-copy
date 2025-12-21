5f610cfc63fe035794fd15cab420e9ebe5641874
Mahesh <maheshzalte2000@gmail.com>
2025-12-21 19:56:42 +0530

    ecosystem file

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { connectDB } from "./config";
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     // lightweight console logger for startup
     console.log(
       `Server listening on http://localhost:${PORT} (env: ${process.env.NODE_ENV || "development"})`,
